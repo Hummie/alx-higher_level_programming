7344edf62a51a2a31664542927a3d7607a618d4d
Hummie <humphreywill77@gmail.com>
2023-09-10 23:13:05 -0700

    Printing matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    num_rows = len(matrix)
+    num_colu = len(matrix[0])
+
+    for row in range(num_rows):
+        for colu in range(num_colu):
+            print("{:2d}".format(matrix[row][colu]),end = ' ')
+        print()
