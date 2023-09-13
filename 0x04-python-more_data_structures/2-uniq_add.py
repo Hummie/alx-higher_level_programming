8f2dced3b0660c7c80cdf8390aa83db05c5b7a1e
Hummie <humphreywill77@gmail.com>
2023-09-13 12:08:45 -0700

    Add unique num

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_list = list(set(my_list))
-    for num in uniq_list:
-        list_sum = sum(uniq_list)
-    return list_sum
+    sum = 0
+    for num in set(my_list):
+        sum += num
+    return sum
