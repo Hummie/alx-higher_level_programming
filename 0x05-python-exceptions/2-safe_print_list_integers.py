c982b7e57eef398b8602fc92ec9303fe61f88a6a
Hummie <humphreywill77@gmail.com>
2023-09-25 09:54:37 -0700

    Elements of list and integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    index = print_int = 0
+    while True:
+        try:
+                if index < x:
+                    print("{:d}".format(my_list[index]), end='')
+                    index += 1
+                    print_int += 1
+                else:
+                    print()
+                    return print_int
+        except (TypeError, ValueError):
+            index += 1
