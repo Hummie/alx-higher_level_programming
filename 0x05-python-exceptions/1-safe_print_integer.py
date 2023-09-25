12302d60346850c81a7c5acbdc2b4871d2ee3424
Hummie <humphreywill77@gmail.com>
2023-09-25 07:25:28 -0700

    Printing integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    while True:
+        try:
+            if isinstance(value, int):
+                print("{:d}".format(value))
+                return True
+            else:
+                return False
+        except:
+            print()
