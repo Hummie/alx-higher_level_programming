c6d6379acdc67b7e1479392664b198ad524d5f56
Hummie <humphreywill77@gmail.com>
2023-10-02 14:31:35 -0700

    Rectangle with get set

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Class defining a rectangle'''
+
+
+class Rectangle:
+    '''A class rectangle'''
+
+    def __init__(self, width=0, height=0):
+        '''Initialisation of the rectangle
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+        '''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''Retrieving and setting width'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Retrieving and setting height.'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
