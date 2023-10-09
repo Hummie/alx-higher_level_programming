9e3ef88a923690f9b59e3f75b2280ce2a31cc618
Hummie <humphreywill77@gmail.com>
2023-10-09 05:47:53 -0700

    Class with method and exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Defines a class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''A class representation'''
+
+    def area(self):
+        '''A method area that hasn't been implemented'''
+        raise Exception("area() is not implemented")
