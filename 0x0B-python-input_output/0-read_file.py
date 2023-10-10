976af63ff810ca0d345989c560dd0c7e9adcbc41
Hummie <humphreywill77@gmail.com>
2023-10-10 04:24:59 -0700

    Reading from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A function that reads and prints a text file'''
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as file_read:
+        read_data = file_read.read()
+        print(read_data)
