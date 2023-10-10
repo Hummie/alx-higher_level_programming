e7dac1cfd73f2aecf9e57635cef94e63fab3978d
Hummie <humphreywill77@gmail.com>
2023-10-10 05:52:31 -0700

    JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module that returns JSON rep of obj string'''
+import json
+
+
+def to_json_string(my_obj):
+    '''Returns json rep of a string
+        Args:
+            my_obj: Object to be returned as json
+        Return:
+            json string
+    '''
+    return(json.dumps(my_obj))
