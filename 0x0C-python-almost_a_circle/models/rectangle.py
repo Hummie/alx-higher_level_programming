d02ae7bccb58b681c31229cc98037f7694a268c0
Hummie <humphreywill77@gmail.com>
2023-10-14 01:12:47 -0700

    String representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -112,3 +112,9 @@ class Rectangle(Base):
         '''Prints rectangle in standard output using #'''
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        '''Returns a string representatin of rectangle'''
+        return ('[rectangle] ({}) {}/{} - {}/{}'
+                .format(self.id, self.__x, self.__y, self.__width,
+                        self.__height))
