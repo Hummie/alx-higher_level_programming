437a0171ca25a7de3bcb62498dd32dfe5f3decc5
Hummie <humphreywill77@gmail.com>
2023-10-16 05:45:21 -0700

    json to fie

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-'''Base module that all functions inherit from''' 
+'''Base module that all functions inherit from'''
 import json
 
 
 class Base:
     '''A class with a private attribute'''
     __nb_objects = 0
-
 
     def __init__(self, id=None):
         '''Initialization of the class'''
@@ -16,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries.
 
@@ -28,3 +28,25 @@ class Base:
         if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): instances who inherits of Base - example:
+            list of Rectangle or list of Square instances.
+        """
+        filename = "{}.json".format(cls.__name__)
+        list_dic = []
+
+        if not list_objs:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
